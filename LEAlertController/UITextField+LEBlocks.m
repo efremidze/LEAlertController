c8197ed4eaba2cdb6480393a11ac4e38c81006d3
brightsider <ibrightsider@gmail.com>
2015-09-09 10:15:34 +0300

    make universal UITextField with didChange block

--- LEAlertController/UITextField+LEBlocks.m
+++ LEAlertController/UITextField+LEBlocks.m
@@ -10,32 +10,9 @@
 
 #import <objc/runtime.h>
 
-static char UITextFieldDelegateKey;
-
 static char UITextFieldDidChangeBlockKey;
 
-@interface UITextField () <UITextFieldDelegate>
-
-@end
-
 @implementation UITextField (LEBlocks)
-
-- (void)checkDelegate
-{
-    if (self.delegate != self) {
-        objc_setAssociatedObject(self, &UITextFieldDelegateKey, self.delegate, OBJC_ASSOCIATION_ASSIGN);
-        self.delegate = self;
-    } else {
-        self.delegate = self;
-    }
-}
-
-- (id<UITextFieldDelegate>)originalDelegate
-{
-    return objc_getAssociatedObject(self, &UITextFieldDelegateKey);
-}
-
-#pragma mark -
 
 - (TextFieldBlock)didChangeBlock
 {
@@ -44,30 +21,10 @@ static char UITextFieldDidChangeBlockKey;
 
 - (void)setDidChangeBlock:(TextFieldBlock)didChangeBlock
 {
-    [self checkDelegate];
-    objc_setAssociatedObject(self, &UITextFieldDidChangeBlockKey, didChangeBlock, OBJC_ASSOCIATION_COPY);
-}
-
-#pragma mark - UITextFieldDelegate
-
-- (void)textFieldDidBeginEditing:(UITextField *)textField {
     [self addTarget:self
              action:@selector(textFieldDidChangeEditing:)
    forControlEvents:UIControlEventEditingChanged];
-    
-    id originalDelegate = [self originalDelegate];
-    if ([originalDelegate respondsToSelector:@selector(textFieldDidBeginEditing:)])
-        [originalDelegate textFieldDidBeginEditing:textField];
-}
-
-- (void)textFieldDidEndEditing:(UITextField *)textField {
-    [self removeTarget:self
-             action:@selector(textFieldDidChangeEditing:)
-   forControlEvents:UIControlEventEditingChanged];
-    
-    id originalDelegate = [self originalDelegate];
-    if ([originalDelegate respondsToSelector:@selector(textFieldDidEndEditing:)])
-        [originalDelegate textFieldDidEndEditing:textField];
+    objc_setAssociatedObject(self, &UITextFieldDidChangeBlockKey, didChangeBlock, OBJC_ASSOCIATION_COPY);
 }
 
 - (void)textFieldDidChangeEditing:(UITextField *)textField {
